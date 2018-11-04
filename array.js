09754d9e708faea38c3a5d5a93c82adb6cb588d6
Artoym <44746890+Flashbuck@users.noreply.github.com>
2018-11-05 02:53:49 +0300

    Create array.js

---
+++ array.js
@@ -0,0 +1,14 @@
+a=[1,2,3,[4,5,6,[7,8,9,[0]]]];
+b=[];
+function Vector(array)
+{
+    for (var i = 0; i < array.length; i++) {
+        if (typeof(array[i]) == 'object' && array[i].length) { 
+            flat(array[i]);
+        } else if(array[i].length!=0){
+            b.push(array[i]);
+        }
+    }
+}
+
+Vector(a);
